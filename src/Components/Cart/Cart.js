9650c8da97632b2763ee0524f5c9c05181eccff6
Barua <ana.paula.barua@accenture.com>
2021-04-22 17:51:07 -0300

    progress

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,17 @@
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = <ul className={classes['cart-items']}>{[
+    { id: "c1", name: "Sushi", amount: 2, price: "12.99" },
+  ].map((item) => <li>{item.name}</li>)}</ul>
+
+  return (
+    <div>
+      cartitems
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Cart;
--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -4,7 +4,7 @@ import MealItemForm from "./MealItemForm";
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
   return (
-    <li>
+    <li className={classes.meal}>
       <div>
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
