890653c117e82add42b2e351f63b9f02d00fa8ad
Barua <ana.paula.barua@accenture.com>
2021-04-22 14:42:32 -0300

    food

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,16 +1,19 @@
 import { Fragment } from 'react';
 import classes from './Header.module.css';
 import mealsImage from '../../Assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 const Header = props=>{
     return (
         <Fragment>
-            <header>
+            <header className={classes.header}>
                 <h1>React meals</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
-            <div>
-                <img src={mealsImage}/>
+            <div className={classes['main-image']}>
+                <img src={mealsImage} alt='food'/>
             </div>
         </Fragment>
     )
-}
+};
+
+export default Header;
